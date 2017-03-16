e6a72f358ab6f373c60f5b14bb44e680c1464ac4
Christian Calle <calle.christian@gmail.com>
2017-03-16 15:37:49 +0000

    work on delete character

--- js/play.js
+++ js/play.js
@@ -15,50 +15,59 @@ var p5;
 var p6;
 var pList = [0,1,2,3,4,5];
 
+var pack1;
+var pack2;
+var pack3;
+var pack4;
+var pack5;
+var pack6;
+var packList = [pack1, pack2, pack3, pack4, pack5, pack6];
+
 var goldText;
 var attackText;
 var lifeText;
+//var deleteX;
 
 var card;
 
 var question;
-
+var test;
 
 var token;
 
 var dice;
-var dieRoll;
+//var dieRoll;
 
+var currentRace;
+var playerNumber = 0;
+var players = [];
 
 
 var playState = {
  
 create: function () {
 
-
+    //Create Game Board
     game.add.sprite(300,0, 'board');
     
-    c1 = game.add.sprite(100, 50, 'sheet');
-    c2 = game.add.sprite(100, 200, 'sheet');
-    c3 = game.add.sprite(100, 350, 'sheet');
+    //Create Character Frames
+    c1 = this.game.add.sprite(100, 50, 'sheet');
+    c2 = this.game.add.sprite(100, 200, 'sheet');
+    c3 = this.game.add.sprite(100, 350, 'sheet');
     
-    c4 = game.add.sprite(1000, 50, 'sheetX');
-    c5 = game.add.sprite(1000, 200, 'sheetX');
-    c6 = game.add.sprite(1000, 350, 'sheetX');
+    c4 = this.game.add.sprite(1000, 50, 'sheetX');
+    c5 = this.game.add.sprite(1000, 200, 'sheetX');
+    c6 = this.game.add.sprite(1000, 350, 'sheetX');
     
     cList = [c1,c2,c3,c4,c5,c6];
     
     TurnOnCharacters();
     
+    //Create Card
+    card = this.game.add.button (75, 500, 'card', ShowQuestion, this, 2,1,0);
     
-    
-    card = game.add.button (75, 500, 'card', ShowQuestion, this, 2,1,0);
-    
-    token = game.add.sprite(350, 50, 'token');
-    token.inputEnabled = true;
-    token.input.enableDrag(false);
-    
-    dice = game.add.sprite(950, 500, 'die');
+    //Create Die    
+    dice = this.game.add.sprite(950, 500, 'die');
     dice.animations.add('roll', [0,1,2,3,4,5], 10, true);
     dice.inputEnabled = true;
     dice.events.onInputDown.add(rollDie, this);
@@ -66,7 +75,7 @@ create: function () {
     
     
     
-    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
   
     this.scale.setScreenSize( true );
 },
@@ -83,7 +92,7 @@ function rollDie() {
     dice.animations.play('roll');
     
     //timer
-    var rollTimer = game.time.create(false);
+    var rollTimer = this.game.time.create(false);
     
     var rollTime = Math.floor(((Math.random() * 3) + 3) * 1000);
     
@@ -106,6 +115,8 @@ function TurnOnCharacters () {
         cList[i].inputEnabled = true;
         cList[i].events.onInputDown.add(AddStats, cList[i]);
         cList[i].name = "c" + (i + 1).toString();
+        
+        packList[i] = game.add.group();
     }
 }
 
@@ -146,20 +157,38 @@ function AddStats(player) {
                 
                 if (player == c1 || player == c2 || player == c3) {
                 
-                    goldText = game.add.text(16, 3, gold, goldStyle);
-                    attackText = game.add.text(9, 46, attack, attackStyle);
-                    lifeText = game.add.text(16, 88, life, lifeStyle);
+                    goldText = this.game.add.text(16, 3, gold, goldStyle);
+                    attackText = this.game.add.text(9, 46, attack, attackStyle);
+                    lifeText = this.game.add.text(16, 88, life, lifeStyle);
+                    var deletePlayer = this.game.add.button(80, -10, 'deleteX', DeletePlayer, this, 2,1,0);
+                   
+                    
                 }
                 else {
                 
-                    goldText = game.add.text(72, 3, gold, goldStyle);
-                    attackText = game.add.text(79, 46, attack, attackStyle);
-                    lifeText = game.add.text(72, 88, life, lifeStyle);
+                    goldText = this.game.add.text(72, 3, gold, goldStyle);
+                    attackText = this.game.add.text(79, 46, attack, attackStyle);
+                    lifeText = this.game.add.text(72, 88, life, lifeStyle);
+                    deletePlayer = this.game.add.button(-20, -10, 'deleteX', DeletePlayer, this, 2,1,0);
+                    
                 }
-        
+                
+                deletePlayer.name = "delete" + player.name;
+                
                 player.addChild(goldText);
                 player.addChild(attackText);
                 player.addChild(lifeText);
+                player.addChild(deletePlayer);
+                
+                packList[i].add(goldText);
+                packList[i].add(attackText);
+                packList[i].add(lifeText);
+                packList[i].add(deletePlayer);
+                
+                
+                deletePlayer.input.priorityID = 10;
+                
+                CreateToken();
                 
                 return;
             }
@@ -184,58 +213,75 @@ function PickRace(player) {
         }
         else {
         
-            var x = 45;
-            var y = 65;
+            x = 45;
+            y = 65;
         }
     
     
     switch (randomChoice) {
         case 0:
-            p1 = game.add.sprite(x, y, 'elf');
+            p1 = this.game.add.sprite(x, y, 'elf');
             player.addChild(p1);
             p1.anchor.set(0.5,0.5);
+            currentRace = "elf";
             break;
         
         case 1:
-            p2 = game.add.sprite(x, y, 'hobbit');
+            p2 = this.game.add.sprite(x, y, 'hobbit');
             player.addChild(p2);
             p2.anchor.set(0.5,0.5);
+            currentRace = "hobbit";
             break;
         
         case 2:
-            p3 = game.add.sprite(x, y, 'knight');
+            p3 = this.game.add.sprite(x, y, 'knight');
             player.addChild(p3);
             p3.anchor.set(0.5,0.5);
+            currentRace = "knight";
             break;
                 
         case 3:
-            p4 = game.add.sprite(x, y, 'ogre');
+            p4 = this.game.add.sprite(x, y, 'ogre');
             player.addChild(p4);
             p4.anchor.set(0.5,0.5);
+            currentRace = "ogre";
             break;
     
         case 4:
-            p5 = game.add.sprite(x, y, 'warrior');
+            p5 = this.game.add.sprite(x, y, 'warrior');
             player.addChild(p5);
             p5.anchor.set(0.5,0.5);
+            currentRace = "warrior";
             break;
         
         case 5:
-            p6 = game.add.sprite(x, y, 'wizard');
+            p6 = this.game.add.sprite(x, y, 'wizard');
             player.addChild(p6);
             p6.anchor.set(0.5,0.5);
+            currentRace = "wizard";
             break;
    
     }
     
 }
 
+function CreateToken() {
+    
+    token = this.game.add.sprite(350, 50, currentRace);
+    token.inputEnabled = true;
+    token.input.enableDrag(false);
+    playerNumber += 1;
+    token.name = currentRace + playerNumber.toString();
+    players.push(token);
+    
+}
+
 function ShowQuestion() {
     
-    question = game.add.sprite(300, 0, 'answersheet');
-    var deleteX = game.add.button (500, 50, 'deleteX', DeleteQuestion, this, 2,1,0);
+    question = this.game.add.sprite(300, 0, 'answersheet');
+    var deleteQuestion = this.game.add.button (500, 50, 'deleteX', DeleteQuestion, this, 2,1,0);
     
-    question.addChild(deleteX);
+    question.addChild(deleteQuestion);
 
 }
 
@@ -243,4 +289,26 @@ function DeleteQuestion() {
     
     question.destroy();
 
+}
+
+function DeletePlayer() {
+    
+    //var text = game.add.text(200,200, this.name, { font: "200px Arial", fill: "#ff0044", align: "center" });
+     
+    //rollDie();
+    // var getPlayer = this.name;
+    // var thisPlayer;
+    // for (var i = 0; i < cList.length; i++) {
+        
+    //     if (i.name == getPlayer ){
+    //         thisPlayer = i;
+    //     }
+        
+        
+    // }
+    
+    //this.removeChildren(0, this.name.length - 1);
+    
+}
+    
 }
