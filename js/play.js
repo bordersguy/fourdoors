2e31d343bdc6899923ebc1867dfc7d5c2604c496
Christian Calle <calle.christian@gmail.com>
2017-03-18 16:31:15 +0000

    work on refresh characters

--- js/play.js
+++ js/play.js
@@ -8,13 +8,6 @@ var cList;
 var cSet = [false, false, false, false, false, false];
 
 var race;
-var p1;
-var p2;
-var p3;
-var p4;
-var p5;
-var p6;
-var pList = [0,1,2,3,4,5];
 
 var pack1;
 var pack2;
@@ -22,26 +15,36 @@ var pack3;
 var pack4;
 var pack5;
 var pack6;
+var refreshPack = [pack1, pack2, pack3, pack4, pack5, pack6];
 var packList = [pack1, pack2, pack3, pack4, pack5, pack6];
 
 var goldText;
 var attackText;
 var lifeText;
-//var deleteX;
+var playerName;
+var deletePlayer;
+
 
 var card;
+var board;
 
 var question;
 var test;
 
-var token;
+var token1;
+var token2;
+var token3;
+var token4;
+var token5;
+var token6;
+var tokenList;
+var tokenName;
 
 var dice;
-//var dieRoll;
 
 var currentRace;
-var playerNumber = 0;
-var players = [];
+var getPlayer;
+
 
 
 var playState = {
@@ -49,7 +52,11 @@ var playState = {
 create: function () {
 
     //Create Game Board
-    game.add.sprite(300,0, 'board');
+    board = this.game.add.sprite(300,0, 'board');
+    
+    tokenList = this.game.add.group();
+    tokenList = [token1, token2, token3, token4, token5, token6];
+    
     
     //Create Character Frames
     c1 = this.game.add.sprite(100, 50, 'sheet');
@@ -63,9 +70,7 @@ create: function () {
     cList = [c1,c2,c3,c4,c5,c6];
     
     TurnOnCharacters();
-    
-    //packList = [pack1, pack2, pack3, pack4, pack5, pack6];
-    
+
     //Create Card
     card = this.game.add.button (75, 500, 'card', ShowQuestion, this, 2,1,0);
     
@@ -165,8 +170,11 @@ function AddStats(player) {
                     goldText = this.game.add.text(16, 3, gold, goldStyle);
                     attackText = this.game.add.text(9, 46, attack, attackStyle);
                     lifeText = this.game.add.text(16, 88, life, lifeStyle);
-                    var deletePlayer = this.game.add.button(80, -10, 'deleteX', DeletePlayer, this, 2,1,0);
-                   
+                    deletePlayer = this.game.add.button(80, -10, 'deleteX', DeletePlayer, this, 2,1,0);
+                    packList[i].add(deletePlayer);
+                    packList[i].add(goldText);
+                    packList[i].add(attackText);
+                    packList[i].add(lifeText);
                     
                 }
                 else {
@@ -175,27 +183,17 @@ function AddStats(player) {
                     attackText = this.game.add.text(79, 46, attack, attackStyle);
                     lifeText = this.game.add.text(72, 88, life, lifeStyle);
                     deletePlayer = this.game.add.button(-20, -10, 'deleteX', DeletePlayer, this, 2,1,0);
-                    
+                    packList[i].add(deletePlayer);
+                    packList[i].add(goldText);
+                    packList[i].add(attackText);
+                    packList[i].add(lifeText);
                 }
-                
-                deletePlayer.name = "delete" + player.name;
-                
-                
-                
-                
-                // player.addChild(goldText);
-                // player.addChild(attackText);
-                // player.addChild(lifeText);
-                // player.addChild(deletePlayer);
-                
-                packList[i].add(goldText);
-                packList[i].add(attackText);
-                packList[i].add(lifeText);
-                packList[i].add(deletePlayer);
-                
+
+                playerName = this.game.add.text(10, 120, currentRace + (i + 1).toString(), { font: "20px Arial", fill: "white", align: "center" }, packList[i]); 
+
                 deletePlayer.input.priorityID = 10;
                 
-                CreateToken();
+                CreateToken(i);
                 
                 return;
             }
@@ -203,14 +201,10 @@ function AddStats(player) {
             
             
         }
-        
-        
-
-    
 }
 
 
-function PickRace(player, pNumber) {
+function PickRace(player, number) {
     var randomChoice = Math.floor(Math.random() * 6);
     
        if (player == c1 || player == c2 || player == c3) {
@@ -229,42 +223,42 @@ function PickRace(player, pNumber) {
     switch (randomChoice) {
         case 0:
             race = this.game.add.sprite(x, y, 'elf');
-            packList[pNumber].add(this.race);
+            packList[number].add(race);
             race.anchor.set(0.5,0.5);
             currentRace = "elf";
             break;
         
         case 1:
             race = this.game.add.sprite(x, y, 'hobbit');
-            packList[pNumber].add(this.race);
+            packList[number].add(race);
             race.anchor.set(0.5,0.5);
             currentRace = "hobbit";
             break;
         
         case 2:
             race = this.game.add.sprite(x, y, 'knight');
-            packList[pNumber].add(this.race);
+            packList[number].add(race);
             race.anchor.set(0.5,0.5);
             currentRace = "knight";
             break;
                 
         case 3:
             race = this.game.add.sprite(x, y, 'ogre');
-            packList[pNumber].add(this.race);
+            packList[number].add(race);
             race.anchor.set(0.5,0.5);
             currentRace = "ogre";
             break;
     
         case 4:
             race = this.game.add.sprite(x, y, 'warrior');
-            packList[pNumber].add(this.race);
+            packList[number].add(race);
             race.anchor.set(0.5,0.5);
             currentRace = "warrior";
             break;
         
         case 5:
             race = this.game.add.sprite(x, y, 'wizard');
-            packList[pNumber].add(this.race);
+            packList[number].add(race);
             race.anchor.set(0.5,0.5);
             currentRace = "wizard";
             break;
@@ -273,15 +267,50 @@ function PickRace(player, pNumber) {
     
 }
 
-function CreateToken() {
+function CreateToken(player) {
+    //outer box = x85.71 y85.71
+    //player is equal to i from AddStats()
     
-    token = this.game.add.sprite(350, 50, currentRace);
-    token.inputEnabled = true;
-    token.input.enableDrag(false);
-    playerNumber += 1;
-    token.name = currentRace + playerNumber.toString();
-    players.push(token);
+    var startSpaceXY = Math.floor(Math.random() * 4);
+    var tokenX;
+    var tokenY;
+    switch (startSpaceXY) {
+        case 0:
+            tokenX = board.x;
+            tokenY = board.y + ((1 * player) * 85.71);
+            break;
+            
+         case 1:
+            tokenX = board.x + ((1 * player) * 85.71);
+            tokenY = board.y;
+            break;
+            
+         case 2:
+            tokenX = board.x + (board.width - 85.71);
+            tokenY = board.y + ((1 * player) * 85.71);
+            break;
+            
+         case 3:
+            tokenX = board.x + ((1 * player) * 85.71);
+            tokenY = board.height - 85.71;
+            break;
+    }
     
+    console.log(startSpaceXY);
+    console.log(tokenX);
+
+    
+    
+    tokenList[player] = this.game.add.sprite(tokenX, tokenY, currentRace);
+    tokenList[player].x = tokenX; 
+    tokenList[player].inputEnabled = true;
+    tokenList[player].input.enableDrag(false);
+
+    tokenList[player].name = currentRace + (player).toString();
+    tokenName = this.game.add.text(0, 0, currentRace + (player + 1).toString(), { font: "20px Arial", fill: "white", align: "center" });
+    tokenList[player].addChild(tokenName);
+    tokenName.y = tokenList[player].height;
+
 }
 
 function ShowQuestion() {
@@ -300,31 +329,31 @@ function DeleteQuestion() {
 }
 
 function DeletePlayer() {
-    
-    //var text = game.add.text(200,200, this.name, { font: "200px Arial", fill: "#ff0044", align: "center" });
-     
-    //rollDie();
-    
-    var getPlayer = parseInt(this.name.substring(1,2)) - 1;
-    
+
+    getPlayer = parseInt(this.name.substring(1,2), 10) - 1;
+    console.log(getPlayer);
     for (var i = 0; i < packList.length; i++) {
         
         if (i == getPlayer ) {
-            rollDie();
-            packList[i].remove(goldText);
-            packList[i].remove(attackText);
-            packList[i].remove(lifeText);
-            packList[i].remove(race);
-            //packList[i].removeBetween(0, 3, true, true);
-            //packList[i].removeChildren(0, packList[i].length - 1);
-            //packList[i].destroy(destroyChrildren, soft);
+            
+           
+            
+            packList[i].getChildAt(1).destroy();
+            
+            packList[i].forEach(function(child) {
+                 
+                 packList[i].removeChild(packList[i].getChildAt(0));
+                     
+            });
+
+            tokenList[i].destroy(true,false);
+            
+            //Allows for new Character
+            cSet[i] = false;
+
             return;
+            
         }
-        
-        
     }
-    
-   
-    
 }
     
